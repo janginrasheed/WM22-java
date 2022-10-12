4152c08f22117acdb571cbb6a352f5861f4e714c
Jangin Rasheed <jangin9rasheed@gmail.com>
2022-10-12 15:48:24 +0200

    Test created

--- src/test/java/com/wm22/Wm22ApplicationTests.java
+++ src/test/java/com/wm22/Wm22ApplicationTests.java
@@ -3,11 +3,30 @@ package com.wm22;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class Wm22ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void checkMatchResult() {
+//		@Autowired
+//		MatchesDao matchesDao = null;
+//		Match match = new Match();
+
+        String firstTeamGoals = "1.5";
+        String secondTeamGoals = "0";
+
+
+        assertTrue(Integer.parseInt(firstTeamGoals) > -1);
+        assertTrue(Integer.parseInt(secondTeamGoals) > -1);
+
+        assertTrue(Integer.parseInt(firstTeamGoals) <= 99);
+        assertTrue(Integer.parseInt(secondTeamGoals) <= 99);
+    }
 
 }
